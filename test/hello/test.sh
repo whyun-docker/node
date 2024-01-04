a1c61d4c9e04bfc02802697063db3875399606f1
yunnysunny <yunnysunny@gmail.com>
2024-01-04 12:42:30 +0800

    add more test with docker exec

--- test/hello/test.sh
+++ test/hello/test.sh
@@ -15,10 +15,10 @@ docker build . -t ${MY_NAME} \
 --build-arg IMAGE_VERSION=${IMAGE_VERSION}
 # test script
 container_id=$(docker run -e APP_PORT="${APP_PORT}" -p ${APP_PORT}:${APP_PORT}  -d ${MY_NAME})
-docker exec -i "${container_id}" node -e "console.log(new Date().toString())"
-# docker exec "${container_id}" ps -ef | grep node
-# sleep 3
-# curl http://localhost:${APP_PORT}
-# docker stop "${container_id}"
-# docker rm "${container_id}"
+docker exec "${container_id}" node -e "console.log(new Date().toString())"
+docker exec "${container_id}" ps -ef | grep node
+sleep 3
+curl http://localhost:${APP_PORT}
+docker stop "${container_id}"
+docker rm "${container_id}"
 
